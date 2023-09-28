fa2c1c397fe7762fc11e0db3dd729a9607765783
Christian Nainggolan <christiannainggolan@gmail.com>
2023-09-29 02:14:26 +0700

    ejs4

--- app.js
+++ app.js
@@ -4,13 +4,25 @@ const port = 3000
 
 app.set("view engine","ejs")
 
+app.get('/', (req, res) => {
+    res.redirect('/registration')
+})
+
 app.get('/registration', (req, res) => {
   res.render('registration')
 })
 
 app.get('/login', (req, res) => {
     res.render('login')
-  })
+})
+
+app.get('/posting', (req, res) => {
+    res.render('postpage')
+})
+
+app.get('/createprofile', (req, res) => {
+    res.render('createprofile')
+})
   
 
 app.listen(port, () => {
