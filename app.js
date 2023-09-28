9c08dcf6823c5b8b89482f0e6f27256ce9454634
Christian Nainggolan <christiannainggolan@gmail.com>
2023-09-28 23:45:29 +0700

    ejs_login_regist

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
+const express = require('express')
+const app = express()
+const port = 3000
 
+app.set("view engine","ejs")
+
+app.get('/registration', (req, res) => {
+  res.render('registration')
+})
+
+app.get('/login', (req, res) => {
+    res.render('login')
+  })
+  
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
